725dfbb8e48dd61fbb5c8aecec963531cad7ad5c
Gregory C. Sullivan <gcsullivan@pm.me>
2022-05-01 00:03:54 -0500

    Update to use ThreadingHttpServer and BaseHTTPRequestHandler

--- server.py
+++ server.py
@@ -1,25 +1,46 @@
-import os
-import random
-
-from http import HTTPStatus
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from json import dumps, loads
+from os import getenv
+from random import randint
 
 
-class MyHandler(SimpleHTTPRequestHandler):
-    server_id = random.randint(1, 100)
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    server_id = randint(1, 100)
 
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-        msg = f'Your magic number is { self.server_id }.'
+    def _send_response(self, body=None):
+        self._set_headers()
 
-        self.wfile.write(msg.encode())
+        if body is not None:
+            self.wfile.write(dumps(body).encode())
+
+    def do_HEAD(self):
+        self._send_response()
+
+    def do_GET(self):
+        response = {'message': f'Welcome to server { self.server_id }.'}
+        self._send_response(response)
+
+    def do_POST(self):
+        content_size = int(self.headers['Content-Length'])
+        content_body = loads(self.rfile.read(content_size))
+
+        # echo the content body back to the client
+        self._send_response(content_body)
 
 
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
+def main():
+    addr, port = '', int(getenv('PORT', 8080))
 
-httpd = TCPServer(('', port), MyHandler)
-httpd.serve_forever()
+    print(f'Server listening on {addr}:{port}')
+
+    httpd = ThreadingHTTPServer((addr, port), HTTPRequestHandler)
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
